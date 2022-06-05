efbddce7717b63cc42ea4f2976aca21b3120a555
User <mavlyan.sadirov@gmail.com>
2022-06-05 11:52:47 +0600

    HomeWork23

---
+++ src/com/company/Main.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        Integer[] array = new Integer[10];
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        LinkedList<Integer> linkedList = new LinkedList<>();
+
+        System.out.println("Array:\tArrayList:\tLinkedList:");
+
+        for (int i = 0; i < 10; i++) {
+            array[i] = random.nextInt(2);
+            arrayList.add(random.nextInt(2));
+            linkedList.add(random.nextInt(2));
+            System.out.println("  " + array[i] + "\t\t\t" + arrayList.get(i) + "\t\t\t" + linkedList.get(i));
+        }
+
+        Arrays.sort(array, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {return o1 - o2;}
+        });
+
+        arrayList.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {return o1 - o2;}
+        });
+
+        linkedList.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {return o1 - o2;}
+        });
+        System.out.println();
+        System.out.println("Sort Array:  "+Arrays.toString(array));
+        System.out.println("Sort ArrayList:  "+Arrays.toString(arrayList.toArray()));
+        System.out.println("Sort LinkedList:  "+Arrays.toString(linkedList.toArray()));
+
+    }
+}
